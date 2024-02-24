86c8f2313e4518d625172ccded9a177d20a74217
Diogo Silva <73388219+clebinho1234@users.noreply.github.com>
2024-02-24 11:24:52 +0000

    Delete unicafe/src directory

--- unicafe/src/main.jsx
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
